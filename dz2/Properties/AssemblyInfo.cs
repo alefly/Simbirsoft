315172566c10709bbae15c9cdc7513d148c7c92a
alefly <alyona.krebs@yandex.ru>
2018-05-21 21:57:26 +0400

    :з

--- dz2/Properties/AssemblyInfo.cs
+++ dz2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("dz")]
+[assembly: AssemblyTitle("dz2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("dz")]
+[assembly: AssemblyProduct("dz2")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("50cf9f8f-8d65-45f8-a322-dd675a7a4d7b")]
+[assembly: Guid("c982417a-381b-4bd6-b2b3-bf7a0a8f207a")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
